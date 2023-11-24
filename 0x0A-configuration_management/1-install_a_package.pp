f3ddfe55423d084214d70d1f770ecbb7f8b6e58a
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-11-24 15:13:47 -0800

    1-install_a_package.pp

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,10 @@
+# installing flask
+package { 'python3-pip':
+ensure  => 'installed'
+}
+exec { 'install flask':
+command => '/usr/bin/pip3 install flask==2.1.0',
+path    => ['/usr/bin'],
+unless  => 'pip3 show flask | grep -q "Version: 2.1.0"',
+require => Package['python3-pip'],
+}
