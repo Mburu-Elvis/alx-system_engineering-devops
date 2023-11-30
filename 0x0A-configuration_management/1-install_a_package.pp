96809ffdeb02162d0009d369ea914c067c417193
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-11-29 18:52:03 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,16 +1,15 @@
 # installing flask
 package { 'python3-pip':
-ensure  => 'installed'
-}
-exec { 'install_werkzeug':
-command => '/usr/bin/pip3 install Werkzeug==2.0.2'.
-path    => '/usr/bin',
-unless  => '/usr/bin/pip3 show Werkzeug | grep -q "Version: 2.0.2"',
-require => Package['python3-pip'],
+ensure  => 'installed',
 }
 exec { 'install_flask':
-command => '/usr/bin/pip3 install flask==2.1.0',
-path    => '/usr/bin',
-unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-require => [Package['python3-pip'], Exec['install_werkzeug']],
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => '/usr/bin',
+  require => Package['python3-pip'],
+}
+exec { 'check_flask_version':
+  command     => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  path        => '/usr/bin',
+  refreshonly => true,
+  subscribe   => Exec['install_flask'],
 }
